457986067bac3f1a14e328eb75d4639ad3346c73
Geoffrey SAUVAGE <geoffrey.sauvage@soprasteria.com>
2023-12-08 10:19:50 +0100

    Rajout de la méthode principale pour faire la boucle dans l'init

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,11 +1,16 @@
 public class FizzBuzz {
-    /* Write a program that prints the numbers from 1 to 100.
+
+
+    public static void main(String args[]) {
+        /* Write a program that prints the numbers from 1 to 100.
     But for multiples of three print “Fizz” instead of the number
     and for the multiples of five print “Buzz”.
     For numbers which are multiples of both three and five print “FizzBuzz”.
      */
+        System.out.println(print());
+    }
 
-    public String print() {
+    public static String print() {
         return "1";
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,12 +1,12 @@
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
 
     @Test
     void doit_retourner_1() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("1", fizzBuzz.print());
+        assertEquals("1", FizzBuzz.print());
     }
 
 }
